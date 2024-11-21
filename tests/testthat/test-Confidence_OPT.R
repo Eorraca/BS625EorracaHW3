d3dd855bc4d34671ec793ee6a24729200d856b23
Eorraca <eorraca@umich.edu>
2024-11-20 21:48:57 -0500

    Update test-Confidence_OPT.R

--- tests/testthat/test-Confidence_OPT.R
+++ tests/testthat/test-Confidence_OPT.R
@@ -1,4 +1,4 @@
-test_that("Confidence_OPT calculates confidence intervals for linear model correctly", {
+test_that("Confidence_OPT calculates confidence intervals for linear model ", {
   data(mtcars)
   modelx <- lm(mpg ~ wt + hp, data = mtcars)
   ci95 <- Confidence_OPT(modelx, level = 0.95)
@@ -15,7 +15,7 @@ expect_upper <- "99.5 %"
 expect_true(all(colnames(ci99) %in% c(expect_lower, expect_upper)))
 })
 
-test_that("Confidence_OPT works for models with a single predictor", {
+test_that("Confidence_OPT works for models with single predictor", {
   data(mtcars)
   modelx_s <- lm(mpg ~ wt, data = mtcars)
   ci_s <- Confidence_OPT(modelx_s, level = 0.95)
